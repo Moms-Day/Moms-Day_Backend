d02f04dd070fcec30eacbba256cb1794e6a45a0a
JeongKyungSeo <jerion7474@gmail.com>
2018-06-12 23:43:59 +0900

    fix assert error

--- Server/tests/views/careworker/test_signup.py
+++ Server/tests/views/careworker/test_signup.py
@@ -11,4 +11,22 @@ def test_signup_success(flask_client, mongodb_set_for_test, info_test_user):
     assert resp.status_code == 201
 
     # Check that if data inserted the database
-    assert CareWorkerModel.objects(id=info_test_user['id']).first() is True
+    assert CareWorkerModel.objects(id=info_test_user['id']).first() is not None
+
+
+def test_id_duplicated(flask_client, mongodb_set_for_test, info_test_user):
+    flask_client.post(
+        '/care/signup',
+        json=info_test_user
+    )
+
+    resp = flask_client.post(
+        '/care/signup',
+        json=info_test_user
+    )
+
+    # status code 409
+    assert resp.status_code == 409
+
+    # check message
+    assert resp.json['msg'] == 'id duplicated'
