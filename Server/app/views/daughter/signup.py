5282a95cf30684ead93fcdefd1109e60c79e7018
JeongKyungSeo <jerion7474@gmail.com>
2018-06-18 23:52:02 +0900

    [Done] daughter signup API

---
+++ Server/app/views/daughter/signup.py
@@ -0,0 +1,52 @@
+from flask import Blueprint, request, Response, abort
+from flask_restful import Api
+from werkzeug.security import generate_password_hash
+from flasgger import swag_from
+
+from app.views import BaseResource, json_required
+
+from app.models.account import DaughterModel
+from app.models.patient import PatientModel
+
+from app.docs.daughter.signup import DAUGHTER_SIGNUP_POST
+
+api = Api(Blueprint(__name__, __name__))
+api.prefix = '/daughter'
+
+
+@api.resource('/signup')
+class DaughterSignup(BaseResource):
+    @swag_from(DAUGHTER_SIGNUP_POST)
+    @json_required({
+        'id': str,
+        'pw': str,
+        'phone_number': str,
+        'name': str,
+        'age': int,
+        'patients': list
+    })
+    def post(self):
+        id = request.json['id']
+
+        if DaughterModel.objects(id=id).first():
+            abort(409)
+
+        new_user = {
+            'id': id,
+            'pw': generate_password_hash(request.json['pw']),
+            'phone_number': request.json['phoneNumber'],
+            'certify_code': request.json['certifyCode'],
+            'name': request.json['name'],
+            'age': request.json['age']
+        }
+
+        user = DaughterModel(**new_user)
+
+        for patient in request.json['patients']:
+            user.parents.append(
+                PatientModel(name=patient['name'], age=patient['age'], gender=patient['gender']).save()
+            )
+
+        user.save()
+
+        return Response('', 201)
