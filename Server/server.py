4ecb09ce08d7582811b5d79e22efdd7b6894f62c
JeongKyungSeo <jerion7474@gmail.com>
2018-07-17 16:56:46 +0900

    change config

--- Server/server.py
+++ Server/server.py
@@ -2,11 +2,11 @@ import os
 
 from app import create_app
 
-from config.dev import DevConfig
-# from config.production import ProductionConfig
+# from config.dev import DevConfig
+from config.production import ProductionConfig
 
 if __name__ == '__main__':
-    app = create_app(DevConfig)
+    app = create_app(ProductionConfig)
 
     if 'SECRET_KEY' not in os.environ:
         print('[WARNING] SECRET_KEY is not exist')
