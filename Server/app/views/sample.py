c8daa7c58ce16579be9592cdff9362904b8170b8
JeongKyungSeo <jerion7474@gmail.com>
2018-07-17 19:47:55 +0900

    [Auto Deploy TEST] 10 try

--- Server/app/views/sample.py
+++ Server/app/views/sample.py
@@ -41,4 +41,4 @@ class Index(BaseResource):
 @api.resource('/deploy')
 class AutoDeploy(BaseResource):
     def get(self):
-        return Response('i want to success auto deploy~!!!!! ㅠ_ㅠ', 200)
+        return Response('i can do it', 200)
