6d2f687fa3992555700a20d6645a5757157e999e
JeongKyungSeo <jerion7474@gmail.com>
2018-07-18 22:33:33 +0900

    [Auto Deploy] 13 try

--- Server/app/views/sample.py
+++ Server/app/views/sample.py
@@ -41,4 +41,4 @@ class Index(BaseResource):
 @api.resource('/deploy')
 class AutoDeploy(BaseResource):
     def get(self):
-        return Response('last chance', 200)
+        return Response('last chance ><', 200)
