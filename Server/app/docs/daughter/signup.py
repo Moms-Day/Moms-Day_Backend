1ab07adf5af596eb08d391d4e793bcb2833bf326
JeongKyungSeo <jerion7474@gmail.com>
2018-08-28 21:35:12 +0900

    Modify - daughter signup

--- Server/app/docs/daughter/signup.py
+++ Server/app/docs/daughter/signup.py
@@ -43,27 +43,6 @@ DAUGHTER_SIGNUP_POST = {
             'in': 'json',
             'type': 'int',
             'required': True
-        },
-        {
-            'name': 'p_name',
-            'description': '노부모 이름',
-            'in': 'json',
-            'type': 'str',
-            'required': False
-        },
-        {
-            'name': 'p_age',
-            'description': '노부모 나이',
-            'in': 'json',
-            'type': 'int',
-            'required': False
-        },
-        {
-            'name': 'p_gender',
-            'description': '노부모 성별',
-            'in': 'json',
-            'type': 'bool',
-            'required': False
         }
     ],
     'responses': {
--- Server/app/views/daughter/signup.py
+++ Server/app/views/daughter/signup.py
@@ -6,7 +6,6 @@ from flasgger import swag_from
 from app.views import BaseResource, json_required
 
 from app.models.account import DaughterModel
-from app.models.patient import PatientModel
 
 from app.docs.daughter.signup import DAUGHTER_SIGNUP_POST
 
@@ -25,25 +24,21 @@ class DaughterSignup(BaseResource):
         'age': int,
     })
     def post(self):
-        id = request.json['id']
+        req = request.json
+        id = req['id']
 
         if DaughterModel.objects(id=id).first():
             abort(409)
-
-        req = request.json
 
         new_user = {
             'id': id,
             'pw': generate_password_hash(req['pw']),
             'phone_number': req['phoneNumber'],
-            'certify_code': req['certifyCode'],
+            # 'certify_code': req['certifyCode'],
             'name': req['name'],
             'age': req['age']
         }
 
-        d = DaughterModel(**new_user).save()
-
-        if req['p_name']:
-            PatientModel(name=req['p_name'], age=req['p_age'], gender=req['p_gender'], daughter=d).save()
+        DaughterModel(**new_user).save()
 
         return Response('', 201)
